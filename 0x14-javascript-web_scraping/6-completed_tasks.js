ebe70bfc72daa9b4e6a8f1a1e3dcd45328f2793d
njuguna199 <kimaninjuguna199@gmail.com>
2023-03-10 07:10:56 -0800

    0x14

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,26 +1,37 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = process.argv[2];
 
-request(url, function (err, response, body) {
+// Check if the API URL was provided as an argument
+if (process.argv.length !== 3) {
+  console.error('Error: Please provide the API URL as an argument.');
+  process.exit(1);
+}
+
+// Get the API URL from the first argument
+const apiUrl = process.argv[2];
+
+// Make a request to the API to get the tasks data
+request(apiUrl, { json: true }, (err, res, data) => {
   if (err) {
-    console.log(err);
-  } else if (response.statusCode === 200) {
-    const completed = {};
-    const tasks = JSON.parse(body);
-    for (const i in tasks) {
-      const task = tasks[i];
-      if (task.completed === true) {
-        if (completed[task.userId] === undefined) {
-          completed[task.userId] = 1;
-        } else {
-          completed[task.userId]++;
-        }
+    console.error(err);
+    process.exit(1);
+  }
+
+  // Create an object to store the number of completed tasks by user ID
+  const completedTasksByUser = {};
+
+  // Loop through the tasks data and count the number of completed tasks for each user
+  data.forEach(task => {
+    if (task.completed) {
+      if (completedTasksByUser[task.userId]) {
+        completedTasksByUser[task.userId]++;
+      } else {
+        completedTasksByUser[task.userId] = 1;
       }
     }
-    console.log(completed);
-  } else {
-    console.log('An error occured. Status code: ' + response.statusCode);
-  }
+  });
+
+  // Print the completed tasks by user ID to the console
+  console.log(completedTasksByUser);
 });
