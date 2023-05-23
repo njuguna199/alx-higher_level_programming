822a91b292f52373e902182c02c39d1858ea0f57
kim <104842563+njuguna199@users.noreply.github.com>
2023-05-24 00:26:03 +0300

    js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,8 +1,9 @@
 #!/usr/bin/node
-const filename = process.argv[2];
-const string = process.argv[3];
-const fs = require('fs');
 
-fs.writeFile(filename, string, function (err) {
-  if (err) throw err;
+const fs = require('fs');
+const file = process.argv[2];
+const string = process.argv[3];
+
+fs.writeFile(file, string, 'utf-8', function (err) {
+  if (err) console.log(err);
 });
